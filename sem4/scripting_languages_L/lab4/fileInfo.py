60761662902001aeb40d1b154ba50b877959e0c3
wasloso <dziunkowski@gmail.com>
2024-04-12 16:55:54 +0200

    added

---
+++ sem4/scripting_languages_L/lab4/fileInfo.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import json
+
+
+def fileInfo():
+    file_path = input()
+    if not os.path.exists(file_path):
+        sys.stderr.write("File does not exist")
+        raise FileNotFoundError
+    if not os.path.isfile(file_path):
+        sys.stderr.write("Not a file")
+        raise ValueError
+    if not os.access(file_path, os.R_OK):
+        sys.stderr.write("No read permission")
+        raise PermissionError
+    if not os.path.splitext(file_path)[1] == ".txt":
+        sys.stderr.write("Not a txt file")
+        raise ValueError
+
+    with open(file_path, "r") as file:
+        output = {}
+        content = file.read()
+
+        output["path"] = os.path.abspath(file_path)
+        output["characters_count"] = len(content.replace("\n", ""))
+        output["words_count"] = len(content.replace("\n", " ").split())
+        output["lines_count"] = content.count("\n")
+        output["most_common_character"] = max(
+            set(content.replace("\n", "")), key=content.count
+        )
+        output["most_common_word"] = max(
+            content.replace("\n", " ").split(), key=content.replace("\n", " ").count
+        )
+        # dodanie listy slow dla kazdego pliku, aby w nastepnym zadaniu poprawnie obliczyc najpopularniejsze slowo i znak
+        output["words"] = [word for word in content.replace("\n", " ").split()]
+        sys.stdout.write(json.dumps(output))
+
+
+if __name__ == "__main__":
+    fileInfo()
---
+++ sem4/scripting_languages_L/lab4/filesStatistics.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import subprocess
+import json
+
+
+def analyzeFiles():
+    dirPath = sys.argv[1]
+    files = os.listdir(dirPath)
+    output = {}
+    list = []
+
+    for file in files:
+        details = subprocess.run(
+            ["python3", "fileInfo.py"],
+            input=f"{dirPath}/{file}\n",
+            text=True,
+            capture_output=True,
+        )
+        if details.returncode == 0:
+            list.append(json.loads(details.stdout))
+    output["analyzed_files_count"] = len(list)
+    output["total_characters"] = sum([file["characters_count"] for file in list])
+    output["total_words"] = sum([file["words_count"] for file in list])
+    output["total_lines"] = sum([file["lines_count"] for file in list])
+    words = [word for file in list for word in file["words"]]
+    output["most_common_character"] = max(
+        set("".join(words).replace("\n", "")), key="".join(words).count
+    )
+    output["most_common_word"] = max(words, key=words.count)
+    sys.stdout.write(json.dumps(output))
+
+
+if __name__ == "__main__":
+    analyzeFiles()
