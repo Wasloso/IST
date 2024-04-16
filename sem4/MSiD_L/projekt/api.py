4d3065ddec86d3a04d196f1fea8bcb7169b4a25b
wasloso <dziunkowski@gmail.com>
2024-04-16 17:38:50 +0200

    updated

--- sem4/MSiD_L/projekt/api.py
+++ sem4/MSiD_L/projekt/api.py
@@ -3,29 +3,43 @@ import json
 from dataclasses import dataclass
 
 
-@dataclass
 class api:
     baseUrl: str = "http://apps.who.int/gho/athena/api/"
     formatter: str = "?format=json"
+    countries: dict = {}
+    dimensions: dict = {}
+    indicators: dict = {}
+
+    def getCountries(self):
+        response = requests.get(f'{self.baseUrl}/COUNTRY{self.formatter}')
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            for entry in data["dimension"][0]["code"]:
+                self.countries[entry["label"]] = entry["display"]
+
+        with open("data/countries.json", "a+", encoding="utf-8") as f:
+            f.truncate(0)
+            json.dump(self.countries, f, indent=4)
 
     def getDimension(self):
         response = requests.get(f"{self.baseUrl}{self.formatter}")
         if response.status_code == 200:
-            data: dict = json.loads(response.text)
-            return data["dimension"]
-        else:
-            return response.status_code
+            self.dimensions = json.loads(response.text)["dimension"]
+        with open("data/dimensions.json", "a+", encoding="utf-8") as f:
+            f.truncate(0)
+            json.dump(self.dimensions, f, indent=4)
+        return self.dimensions
 
     def getIndicators(self, dimension: str):
         response = requests.get(f"{self.baseUrl}/{dimension}{self.formatter}")
         if response.status_code == 200:
             responseData: dict = json.loads(response.text)
-            indicators: dict = {}
             for entry in responseData["dimension"][0]["code"]:
-                indicators[entry["label"]] = entry["display"]
-            return indicators
-        else:
-            return {}
+                self.indicators[entry["label"]] = entry["display"]
+        with open("data/indicators.json", "a+", encoding="utf-8") as f:
+            f.truncate(0)
+            json.dump(self.indicators, f, indent=4)
+        return self.indicators
 
     def getValues(self, dimension: str, indicator: str):
         response = requests.get(
@@ -52,6 +66,9 @@ class api:
                 )
                 values[country] = value
             display = responseData["dimension"][0]["code"][0]["display"]
+        for country in self.countries:
+            if country not in values.keys():
+                values[country] = None
         return {"indicator": indicator, "display": display, "values": values}
 
     def writeIndicatorValues(self, data: dict):
@@ -67,7 +84,4 @@ class api:
 if __name__ == "__main__":
     api = api()
 
-    indicators = api.getIndicators("GHO")
-    for indicator in indicators.keys():
-        data = api.getValues("GHO", f"{indicator}")
-        api.writeIndicatorValues(data)
+    indicators = api.getCountries()
