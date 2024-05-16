69637c3baeb71d42b6fc21033bb45399f63d990f
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2024-05-16 11:11:53 +0200

    init

--- sem4/MSiD_L/projekt/api.py
+++ sem4/MSiD_L/projekt/api.py
@@ -1,6 +1,8 @@
 import requests
 import json
 from dataclasses import dataclass
+import sys
+import os
 
 
 class api:
@@ -11,11 +13,20 @@ class api:
     indicators: dict = {}
 
     def getCountries(self,write=False) -> dict:
+        if os.path.exists(f'data/countries.json'):
+            with open(f'data/countries.json', "r", encoding="utf-8") as f:
+                self.countries = json.load(f)
+                return self.countries
+            
         response = requests.get(f'{self.baseUrl}/COUNTRY{self.formatter}')
+
         if response.status_code == 200:
             data = json.loads(response.text)
             for entry in data["dimension"][0]["code"]:
                 self.countries[entry["label"]] = entry["display"]
+        else:
+            print('No response')
+
         if write:
              with open("data/countries.json", "a+", encoding="utf-8") as f:
                 f.truncate(0)
@@ -24,9 +35,13 @@ class api:
 
 
     def getDimension(self,write=False) -> dict:
+
         response = requests.get(f"{self.baseUrl}{self.formatter}")
         if response.status_code == 200:
             self.dimensions = json.loads(response.text)["dimension"]
+        else:
+            print('No response')
+            return
         if write:
             with open("data/dimensions.json", "a+", encoding="utf-8") as f:
                 f.truncate(0)
@@ -39,6 +54,9 @@ class api:
             responseData: dict = json.loads(response.text)
             for entry in responseData["dimension"][0]["code"]:
                 self.indicators[entry["label"]] = entry["display"]
+        else:
+            print('No response')
+            return
         if wirte:
             with open("data/indicators.json", "a+", encoding="utf-8") as f:
                 f.truncate(0)
@@ -46,6 +64,16 @@ class api:
         return self.indicators
 
     def getValues(self, indicator: str,wirte=False) -> dict:
+        '''Get the values of a specific indicator, if wirte is True, it will write the data to a json file.\n
+           If data has been already written, retrieve data from a file
+           To get list of indicators, use getIndicators('GHO') method'''
+        if not self.countries:
+            self.getCountries()
+        
+        if os.path.exists(f'data/{indicator}.json') and not wirte:
+            with open(f'data/{indicator}.json', "r", encoding="utf-8") as f:
+                return json.load(f)
+
         response = requests.get(
             f"{self.baseUrl}/GHO/{indicator}{self.formatter}"
         )
@@ -63,17 +91,20 @@ class api:
                         break
                 if not country:
                     continue
-                value = (
-                    entry["value"]["numeric"]
-                    if entry["value"]["numeric"]
-                    else entry["value"]["display"] if entry["value"]["display"]!='No data' else None
-                )
+                if entry["value"]["numeric"]:
+                    value = entry["value"]["numeric"]
+                else:
+                    try:
+                        value = float(entry["value"]["display"])
+                    except ValueError:
+                        value = None
                 values[country] = value
             display = responseData["dimension"][0]["code"][0]["display"]
         if wirte:
             for country in self.countries:
                 if country not in values.keys():
                     values[country] = None
+            self.writeIndicatorValues({"indicator": indicator, "display": display, "values": values})
         return {"indicator": indicator, "display": display, "values": values}
 
     def writeIndicatorValues(self, data: dict) -> None:
@@ -82,9 +113,12 @@ class api:
         with open(f'data/{data["indicator"]}.json', "a+", encoding="utf-8") as f:
             f.truncate(0)
             json.dump(data, f, indent=4)
-        with open("data/avaliable.txt", "a") as f:
+        with open("data/avaliable_2.txt", "a") as f:
             f.write(f'{data['indicator']} - {data["display"]}\n')
 
 
 if __name__ == "__main__":
-    pass
+    api1 = api()
+    countries = api1.getCountries()
+    api1.getValues('SA_0000001688',wirte=True)
+        
--- sem4/MSiD_L/projekt/dataGetter.py
+++ sem4/MSiD_L/projekt/dataGetter.py
@@ -8,37 +8,11 @@ from sklearn import preprocessing
 
 def getData():
     _api = api()
-    countries = _api.getCountries()
-    life_expectancy = _api.getValues("WHOSIS_000001")
-    pop_total = _api.getValues("RS_1845")
-    alcohol_per_capita = _api.getValues("SA_0000001688")
-    doctors_per_10k = _api.getValues("HWF_0001")
-    hdi = _api.getValues("CCO_2")
-    vit_A = _api.getValues("vita")
-    comb = combineDicts(
-        [
-            life_expectancy,
-            pop_total,
-            alcohol_per_capita,
-            doctors_per_10k,
-            hdi,
-            vit_A,
-        ],
-        countries,
-    )
-    df = pd.DataFrame.from_dict(comb)
-    df = df[df["Life expectancy at birth (years)"] != 0]
-    df.rename(
-        {
-            "Life expectancy at birth (years)": "Life Expectancy",
-            '"Alcohol, total per capita (15+) consumption (in litres of pure alcohol) (SDG Indicator 3.5.2)"': "Alcohol pc",
-            "Medical doctors (per 10 000 population)": "Doctors per 10k",
-            "Human development index rank": "HDIR",
-            "Children aged 6-59 months who received vitamin A supplementation (%)": "Vitamin A",
-        },
-        axis=1,
-        inplace=True,
-    )
+    # seatbelts = _api.getValues("RS_209")
+    # print(seatbelts)
+    income_level = _api.getValues("RS_576")
+    print(income_level)
+
     # df.drop(columns="Country", inplace=True)
     # X = df.drop("Life Expectancy", axis=1)
     # y = df["Life Expectancy"]
@@ -47,9 +21,6 @@ def getData():
     # model.fit(X_train, y_train)
     # model.predict(X_test)
     # print(model.score(X_test, y_test))
-
-    df.to_csv("data.csv", sep=",", encoding="utf-8")
-    print(df)
 
 
 def combineDicts(dataList: list[dict], countries: dict):
@@ -60,6 +31,14 @@ def combineDicts(dataList: list[dict], countries: dict):
         for data in dataList:
             countryData[data["display"]] = data["values"].get(country, 0)
         combined.append(countryData)
+    return combined
+
+
+def combineIndicators(countries: dict, indicators: list[dict]):
+    combined = {k: {} for k in countries.values()}
+    for indicator in indicators:
+        for country, value in indicator["values"].items():
+            combined[countries[country]][indicator["display"]] = value
     return combined
 
 
