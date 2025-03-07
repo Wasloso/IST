8fc49a1a636d2e1bf411a191368ddc7ae00ff2c1
Patryk Łuszczek <52531090+Wasloso@users.noreply.github.com>
2025-03-07 20:06:49 +0100

    add: enhance GrayRings image generation with optional blur effect

--- sem6/CG_L/lab1/src/GrayRings.java
+++ sem6/CG_L/lab1/src/GrayRings.java
@@ -19,18 +19,36 @@ public class GrayRings {
     }
 
     static BufferedImage fromParams(int x_res, int y_res, int width) {
+        return fromParams(x_res, y_res, width, true);
+    }
+
+    static BufferedImage fromParams(int x_res, int y_res, int width, boolean blur) {
         BufferedImage image = new BufferedImage(x_res, y_res,
                 BufferedImage.TYPE_INT_RGB);
 
         int x_c = x_res / 2;
         int y_c = y_res / 2;
+        int black = Utils.int2RGB(0, 0, 0);
+        int white = Utils.int2RGB(255, 255, 255);
 
         for (int y = 0; y < y_res; y++) {
             for (int x = 0; x < x_res; x++) {
                 double d = Math.sqrt((y - y_c) * (y - y_c) + (x - x_c) * (x - x_c));
-                int intensity = (int) Utils.intensity(d, width);
-                int gray = Utils.int2RGB(intensity, intensity, intensity);
-                image.setRGB(x, y, gray);
+                if (blur) {
+                    int intensity = (int) Utils.intensity(d, width);
+                    int color = Utils.int2RGB(intensity, intensity, intensity);
+                    image.setRGB(x, y, color);
+
+                } else {
+                    int r = (int) d / width;
+                    if (r % 2 == 0) {
+                        image.setRGB(x, y, black);
+                    } else {
+                        image.setRGB(x, y, white);
+                    }
+
+                }
+
             }
         }
         return image;
